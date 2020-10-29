f1b25737fad42e0f99d323bf056f3fc3c818506a
Gustavo Pereira da Fonseca <39752736+Gustavo053@users.noreply.github.com>
2020-10-28 23:28:41 -0300

    Digital writing added

--- js/input_value.js
+++ js/input_value.js
@@ -1,5 +1,6 @@
 let btStandardSelect = document.querySelectorAll('.bt-standard-select');
 let btActionSelect = document.querySelectorAll('.bt-action-select')
+let valueDigital = document.querySelectorAll('.bt-digital-value');
 
 //Adiciona a classe no botão ativado (botão selecionado) do tipo de 
 //dado que irá ser manuseado (analógico digital)
@@ -22,3 +23,15 @@ btActionSelect.forEach(element => {
         element.classList.add('bt-selected');
     });
 });
+
+//Adiciona a classe no valor escolhido pelo usuário na escrita digital
+//HIGH or LOW (1 ou 0 / ligado ou desligado)
+valueDigital.forEach(element => {
+    element.addEventListener('click', () => {
+        for(let i = 0; i < valueDigital.length; i++) {
+            valueDigital[i].classList.remove('bt-selected');
+        }
+        element.classList.add('bt-selected');
+    });
+});
+
